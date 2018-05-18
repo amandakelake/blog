6210db0beb85c00f13589240798af210e4cd47a0
罗广聪 <930007017@qq.com>
2018-05-18 11:31:00 +0800

    docker 运行node app成功

--- engineering/docker/docker-node-app/server.js
+++ engineering/docker/docker-node-app/server.js
@@ -4,13 +4,13 @@ const express = require('express');
 
 // 官方用的是0.0.0.0地址，指通配地址
 // 127.0.0.1地址代表本地地址，也就是localhost,一般是测试所用
-const PORT = 8080;
-const HOST = '127.0.0.1';
+const PORT = 3000;
+const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
   res.send('I am Docker-node-app');
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT);
 console.log(`Running on http://${HOST}:${PORT}`);
