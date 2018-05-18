b4754ee2ff362c7d96ac91a63e15ceadc674ada8
罗广聪 <930007017@qq.com>
2018-05-18 09:31:27 +0800

    change port host

--- engineering/docker/docker-node-app/server.js
+++ engineering/docker/docker-node-app/server.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// 注意这里，官方是8080端口和0.0.0.0地址，但很多人的8080端口可能已经被占用了
+// 所以直接改为最通用的3000端口和127.0.0.1地址即可
+const PORT = 3000;
+const HOST = '127.0.0.1';
 
 const app = express();
 app.get('/', (req, res) => {
