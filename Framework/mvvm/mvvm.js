7a576ce2b4a48b37a53765657816cecb72032578
罗广聪 <930007017@qq.com>
2018-07-04 23:20:44 +0800

    mvvm

---
+++ Framework/mvvm/mvvm.js
@@ -0,0 +1,72 @@
+class Dep {
+  constructor() {
+    this.subs = []
+  }
+  addSub(sub) {
+    this.subs.push(sub)
+  }
+  notify() {
+    this.subs.forEach(sub => {
+      sub.update()
+    })
+  }
+}
+Dep.target = null
+
+function update(value) {
+  document.querySelector('div').innerText = value
+}
+
+class Watcher {
+  constructor(obj, key, cb) {
+    Dep.target = this
+    this.cb = cb
+    this.obj = obj
+    this.key = key
+    this.value = obj[key]
+    Dep.target = null
+  }
+  update() {
+    this.value = this.obj[this.key]
+    this.cb(this.value)
+  }
+}
+
+function observe(obj) {
+  // 判断类型
+  if (!obj || typeof obj !== 'object') {
+    return
+  }
+  Object.keys(data).forEach(key => {
+    defineReactive(data, key, data[key])
+  })
+}
+
+function defineReactive(obj, key, val) {
+  // 递归子属性
+  observe(val)
+  let dp = new Dep()
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    get: function reactiveGetter() {
+      console.log('get value')
+      // 此处添加新增依赖代码
+      if (Dep.target) {
+        dp.addSub(Dep.target)
+      }
+      return val
+    },
+    set: function reactiveSetter(newVal) {
+      console.log('change value')
+      // 此处添加触发依赖代码
+      val = newVal
+      dp.notify()
+    }
+  })
+}
+
+var data = { name: 'yck' }
+observe(data)
+new Watcher(data, 'name', update)
+data.name = 'yyy' // -> change value
