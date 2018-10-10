60d0c829321c2466756b7e19ef90a3968f5f0ab0
皛白 <930007017@qq.com>
2018-10-10 20:52:23 +0800

    单例模式

--- JS/design-pattern/webpack.dev.config.js
+++ JS/design-pattern/webpack.dev.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/question-car.js",
+  entry: "./src/5-单例模式.js",
   output: {
     path: __dirname,
     filename: "./release/bundle.js"
