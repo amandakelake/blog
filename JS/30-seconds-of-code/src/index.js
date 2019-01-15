53cfe235c691d525d29ac26f282b72eb2cbfa60f
皛白 <930007017@qq.com>
2019-01-15 10:37:49 +0800

    copyToClipboard

--- JS/30-seconds-of-code/src/index.js
+++ JS/30-seconds-of-code/src/index.js
@@ -1,7 +1,13 @@
-import { arrayToHtmlList } from "../util/browser"
+import { arrayToHtmlList, copyToClipboard } from "../util/browser"
 
 // arrayToHtmlList
 document.getElementById("arrayToHtmlList").addEventListener('click', () => {
   const arr = ['item1', 'item2', 'item2']
   arrayToHtmlList(arr, 'ul-list');
+})
+
+// copyToClipboard
+document.getElementById("copyToClipboard").addEventListener('click', () => {
+  const str = document.getElementById("copyText").innerText.toString();
+  copyToClipboard(str);
 })
--- JS/30-seconds-of-code/util/browser.js
+++ JS/30-seconds-of-code/util/browser.js
@@ -1,6 +1,37 @@
+/*
+  将一个数组以li标签的形式插入到id为listID的元素内
+*/
 export const arrayToHtmlList = (arr, listID) => (
   el => (
     (el = document.querySelector(`#${listID}`)),
     (el.innerHTML += arr.map(item => `<li>${item}</li>`).join(''))
   )
 )()
+
+/*
+    页面底部是否在视窗内
+*/
+export const bottomVisible = () => {
+  return document.documentElement.clientHeight + window.scrollY >= (document.documentElement.scrollHeight || document.documentElement.clientHeight)
+}
+
+// 复制到粘贴板 生成<textarea> -> 赋值 -> Selection.getRangeAt()存储值 -> document.execCommand('copy')复制到粘贴板 ->
+export const copyToClipboard = (str) => {
+  // 生成<textarea> -> 赋值
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+  alert('复制成功')
+}
