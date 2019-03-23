73380c2b6527be2be3b8e615ca8c01a2da22419c
皛白 <930007017@qq.com>
2019-03-23 19:43:51 +0800

    change mvvm.js

--- Framework/mvvm/mvvm.js
+++ Framework/mvvm/mvvm.js
@@ -1,72 +1,73 @@
+// 写成单独的数据观察方法，方便递归
+function observe(data) {
+  // 如果不是对象，直接return
+  if (!data || typeof data !== 'object') {
+    return;
+  }
+  Object.keys(data).forEach(key => {
+    defineReactive(data, key);
+  })
+}
+
+// 数据劫持
+function defineReactive(data, key) {
+  let val = data[key];
+
+  // 递归子属性，深度劫持对象
+  observe(val);
+
+  Object.defineProperty(data, key, {
+    enumerable: true,
+    configurable: true,
+    get() {
+      // 注意这里
+      // 在vue源码里面，这里其实是收集依赖的时机，但是在这个时候数据的getter函数不知道当前的Watcher是哪一个
+      // 这里，我们直接放到Watcher构造函数里面做了
+      // 这里下面会使用一个类似全局变量的Dep.target = this来记录当前的Watcher，方便添加依赖到正在执行的Watcher
+      return val;
+    },
+    set(newVal) {
+      if (val === newVal) {
+        return;
+      }
+      val = newVal;
+      // 设置为新值后，把新的值也定义为属性
+      observe(newVal)
+    }
+  })
+}
+
+// 依赖收集
 class Dep {
   constructor() {
     this.subs = []
   }
   addSub(sub) {
+    // 每个sub其实就是一个Watcher实例，自带update方法
     this.subs.push(sub)
   }
   notify() {
     this.subs.forEach(sub => {
-      sub.update()
+      sub.update();
     })
   }
 }
-Dep.target = null
-
-function update(value) {
-  document.querySelector('div').innerText = value
-}
 
 class Watcher {
-  constructor(obj, key, cb) {
-    Dep.target = this
-    this.cb = cb
-    this.obj = obj
-    this.key = key
-    this.value = obj[key]
-    Dep.target = null
+  constructor(data, key, cb) {
+    // 注意这里，这就是上面说的getter里面收集依赖，
+    Dep.target = this;
+    this.data = data;
+    this.key = key;
+    this.cb = cb;
+    this.value = data[key];
+    // 清空了Dep.target，是为了防止notify触发时，不停的绑定Watcher与Dep，造成代码死循环
+    // 因为notify方法会重新调用getter（重新获取值，重新收集依赖）
+    Dep.target = null;
   }
+  // new出来的每个watcher实例都自带这个update方法
   update() {
-    this.value = this.obj[this.key]
-    this.cb(this.value)
+    this.value = this.data[this.key];
+    this.cb(this.value);
   }
 }
-
-function observe(obj) {
-  // 判断类型
-  if (!obj || typeof obj !== 'object') {
-    return
-  }
-  Object.keys(data).forEach(key => {
-    defineReactive(data, key, data[key])
-  })
-}
-
-function defineReactive(obj, key, val) {
-  // 递归子属性
-  observe(val)
-  let dp = new Dep()
-  Object.defineProperty(obj, key, {
-    enumerable: true,
-    configurable: true,
-    get: function reactiveGetter() {
-      console.log('get value')
-      // 此处添加新增依赖代码
-      if (Dep.target) {
-        dp.addSub(Dep.target)
-      }
-      return val
-    },
-    set: function reactiveSetter(newVal) {
-      console.log('change value')
-      // 此处添加触发依赖代码
-      val = newVal
-      dp.notify()
-    }
-  })
-}
-
-var data = { name: 'yck' }
-observe(data)
-new Watcher(data, 'name', update)
-data.name = 'yyy' // -> change value
